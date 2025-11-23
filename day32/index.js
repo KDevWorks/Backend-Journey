f3c3017a88adc0918102b95ea83e111a0cd42a44
Krushna Dasarwad <krushnadasarwad2@gmail.com>
2025-11-23 13:43:20 +0530

    change

--- day32/index.js
+++ day32/index.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
     res.send("Hello from home");
 });
 
-
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
