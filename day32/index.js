d34296be5040ba65651d8b4c50d0d6fa56527562
Krushna Dasarwad <krushnadasarwad2@gmail.com>
2025-12-26 15:52:26 +0530

    change

--- day32/index.js
+++ day32/index.js
@@ -1,39 +1,48 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello from home");
-});
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+// For handling file and directory paths
+const path = require("path");
 
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/views"));
+// Serve static files from the "public" directory
+app.use(express.static("public/css"));
+app.use(express.static(path.join(__dirname, "public/js")));
 
-app.get("/home", (req, res) => {
-    res.render("home.ejs");
-});
-
-app.get("/dice", (req, res) => {
-    let val = Math.floor(Math.random() * 6) + 1;
-    res.render("diceval", { val });
-});
-
-app.get("/ig/:username/:password", (req, res) => {
-    let { username } = req.params;
-    console.log(username);
-    let followers = ["danny","arya", "sansa", "rob", "belish"];
-    res.render("instagram", {username,followers});
-    // res.render("insta",)
-})
-
+// Set the views directory
+app.set("views", path.join(__dirname, "views"));
 
 
 app.listen(port, () => {
-    console.log("Listening no port: ", port);
+  console.log(`Server is running on http://localhost:${port}`);
 });
 
-app.get("/",(req,res)=>{
-    console.log("reqest was sent from local fort");
+app.get('/', (req, res) => {
+  console.log("Home Page Requested");
+  res.render("home.ejs")
+});
+
+app.get("/diceroll", (req, res) => {
+  const num = Math.floor(Math.random() * 6) + 1;
+  res.render("rolldice.ejs", { num });
+})
+
+app.get("/ig/:username", (req, res) => {
+  let followers = ["allen", "bob", "charlie", "david", "eve", "frank"];
+  const { username } = req.params;
+  console.log(username);
+  res.render("instagram.ejs", { username, followers });
+})
+
+app.get("/posts/:id", (req, res) => {
+  const { id } = req.params;
+  // console.log(id);
+
+  const instaData = require("./data.json");
+  const data = instaData[id];
+
+  res.render("instagram2", { userdata: data })
+  // console.log(data);
 })
